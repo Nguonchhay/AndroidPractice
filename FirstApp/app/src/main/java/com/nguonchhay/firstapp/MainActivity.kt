6278d7f92f3f72f117fcb00915e0c05e0f2a5415
Nguonchhay <nguonchhay.touch@gmail.com>
2021-08-17 23:05:10 +0700

    [FirstApp] View Binding demo

--- FirstApp/app/src/main/java/com/nguonchhay/firstapp/MainActivity.kt
+++ FirstApp/app/src/main/java/com/nguonchhay/firstapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.nguonchhay.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.nguonchhay.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val btnOldButton: Button = findViewById<Button>(R.id.btnOldImp)
+        btnOldButton.setOnClickListener {
+            Toast.makeText(this, "Old Style", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnNewImp.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
