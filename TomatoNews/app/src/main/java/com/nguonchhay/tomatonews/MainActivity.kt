06d06cf0dd6fe7e931c110b57eadda9f44193254
Nguonchhay <nguonchhay.touch@gmail.com>
2021-09-04 10:22:49 +0700

    [TOMATONEWS]

--- TomatoNews/app/src/main/java/com/nguonchhay/tomatonews/MainActivity.kt
+++ TomatoNews/app/src/main/java/com/nguonchhay/tomatonews/MainActivity.kt
@@ -2,10 +2,30 @@ package com.nguonchhay.tomatonews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    var tabContainer: TabLayout? = null
+    var fragmentContainer: ViewPager2? = null
+    var fragmentAdapter: FragmentAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabContainer = findViewById(R.id.tabContainer)
+        fragmentContainer = findViewById(R.id.fragmentContainer)
+
+        supportFragmentManager.commit {
+            replace<ExampleFragment>(R.id.fragmentContainer)
+            setReorderingAllowed(true)
+            addToBackStack("name") // name can be null
+        }
+
+        val fragment: ExampleFragment =
+            supportFragmentManager.findFragmentByTag("tag") as ExampleFragment
     }
 }
