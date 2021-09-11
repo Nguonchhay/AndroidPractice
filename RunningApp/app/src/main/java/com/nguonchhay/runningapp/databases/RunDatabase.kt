82cc3054a25f6f60d16102a7aeba4badba9fa0e0
Nguonchhay <nguonchhay.touch@gmail.com>
2021-09-11 19:04:55 +0700

    [RunningApp] Config Room db and converter

---
+++ RunningApp/app/src/main/java/com/nguonchhay/runningapp/databases/RunDatabase.kt
@@ -0,0 +1,14 @@
+package com.nguonchhay.runningapp.databases
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.nguonchhay.runningapp.databases.DAO.RunDAO
+import com.nguonchhay.runningapp.databases.models.Run
+
+
+@Database(entities = [Run::class], version = 1)
+@TypeConverters(BitmapConverter::class)
+abstract class RunDatabase : RoomDatabase() {
+    abstract fun getRunDao(): RunDAO
+}
