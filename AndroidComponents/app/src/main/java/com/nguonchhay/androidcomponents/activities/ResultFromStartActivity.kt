980e661b1ccb6e0303a7dfa2dcdae06d6929438a
Nguonchhay <nguonchhay.touch@gmail.com>
2021-09-19 19:41:48 +0700

    [AC] Start another activity for result

--- AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/ResultFromStartActivity.kt
+++ AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/ResultFromStartActivity.kt
@@ -1,7 +1,10 @@
 package com.nguonchhay.androidcomponents.activities
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nguonchhay.androidcomponents.contracts.ResultFromActivityContract
 import com.nguonchhay.androidcomponents.databinding.ActivityResultFromStartBinding
 
 class ResultFromStartActivity : AppCompatActivity() {
@@ -12,5 +15,11 @@ class ResultFromStartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityResultFromStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSendMessage.setOnClickListener {
+            val result = Intent().putExtra(ResultFromActivityContract.MESSAGE, "Message from ResultFromStart")
+            setResult(Activity.RESULT_OK, result)
+            finish()
+        }
     }
 }
--- AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/StartForResultActivity.kt
+++ AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/StartForResultActivity.kt
@@ -2,17 +2,22 @@ package com.nguonchhay.androidcomponents.activities
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.nguonchhay.androidcomponents.contracts.ResultFromActivityContract
 import com.nguonchhay.androidcomponents.databinding.ActivityStartForResultBinding
 
 private var REQUEST_CODE_PERMISSION = 123
 
 class StartForResultActivity : AppCompatActivity() {
+
+    private val LOG_TAG = "AC LOG: "
 
     lateinit var binding: ActivityStartForResultBinding
 
@@ -29,17 +34,18 @@ class StartForResultActivity : AppCompatActivity() {
         binding.btnPermission.setOnClickListener {
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 binding.lblPermission.text = "Permission is GRANT!"
-                Log.d("OHLALA => ", "binding.btnPermission.setOnClickListener => GRANT")
+                Log.d(LOG_TAG, "binding.btnPermission.setOnClickListener => GRANT")
             } else {
                 requestPermissions(arrayOf(android.Manifest.permission.CAMERA), REQUEST_CODE_PERMISSION)
-                Log.d("OHLALA => ", "requestPermissions")
+                Log.d(LOG_TAG, "requestPermissions")
             }
         }
 
+        // Gallery
         val getImage = registerForActivityResult(
             ActivityResultContracts.GetContent(),
             ActivityResultCallback {
-                Log.d("OHLALA => ", "ActivityResultCallback")
+                Log.d(LOG_TAG, "ActivityResultCallback")
                 binding.imgSelectedItem.setImageURI(it)
             }
         )
@@ -47,9 +53,35 @@ class StartForResultActivity : AppCompatActivity() {
         binding.btnStartGallery.setOnClickListener {
             getImage.launch("image/*")
         }
+        // End Gallery
+
+        // Camera
+
+        val takePhoto = registerForActivityResult(
+            ActivityResultContracts.TakePicture(),
+            ActivityResultCallback {
+                if (it) {
+                    Log.d(LOG_TAG, "ActivityResultContracts.TakePicture()")
+                }
+            }
+        )
+
+        binding.btnTakePicture.setOnClickListener {
+            takePhoto.launch(Uri.parse("acimages"))
+        }
+
+        // End Camera
+
+        // Start another activity for result
+        val resultLauncher = registerForActivityResult(
+            ResultFromActivityContract(),
+            ActivityResultCallback {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        )
 
         binding.btnStartActivityForResult.setOnClickListener {
-            val intent = Intent(this, ResultFromStartActivity::class.java)
+            resultLauncher.launch(null)
         }
 
     }
