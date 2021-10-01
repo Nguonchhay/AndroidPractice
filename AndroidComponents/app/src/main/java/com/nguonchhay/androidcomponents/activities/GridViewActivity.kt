e27319c02df541c1921cef17255310e14c68ed73
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2021-10-01 14:59:33 +0700

    [AC] ActionBar with menu

--- AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/GridViewActivity.kt
+++ AndroidComponents/app/src/main/java/com/nguonchhay/androidcomponents/activities/GridViewActivity.kt
@@ -2,6 +2,9 @@ package com.nguonchhay.androidcomponents.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.nguonchhay.androidcomponents.R
 import com.nguonchhay.androidcomponents.adapters.GridViewAdapter
 import com.nguonchhay.androidcomponents.databinding.ActivityGridViewBinding
@@ -15,6 +18,11 @@ class GridViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityGridViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Toolbar
+        binding.gridViewToolbar.title = ""
+        //setActionBar(binding.gridViewToolbar)
+        setSupportActionBar(binding.gridViewToolbar)
 
         val gridViewData = mutableListOf<GridViewData>(
             GridViewData(R.drawable.minion1, "Minion 1"),
@@ -31,5 +39,19 @@ class GridViewActivity : AppCompatActivity() {
             finish()
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.grid_view_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menuGridViewSearch -> Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show()
+            R.id.menuGridViewSetting -> Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 }
